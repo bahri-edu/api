79db44c00e33903a5c89709ab69e61909667505b
yaseenomer <yaseeno373@gmail.com>
2023-03-21 14:43:45 +0200

    vevevevevv

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import "express-async-errors";
 const dbUser = "yaseenomer";
 const dbPass = "oqwB6M37QrPPLCsr";
 
-const mongoDbUrl = `mongodb://127.0.0.1:27017/bahri-website`;
-// const mongoDbUrl = `mongodb+srv://${dbUser}:${dbPass}@yaseen-cluster.xotylcw.mongodb.net/?retryWrites=true&w=majority`;
+// const mongoDbUrl = `mongodb://127.0.0.1:27017/bahri-website`;
+const mongoDbUrl = `mongodb+srv://${dbUser}:${dbPass}@yaseen-cluster.xotylcw.mongodb.net/?retryWrites=true&w=majority`;
 (async () => {
   try {
     await mongoose.connect(mongoDbUrl);
